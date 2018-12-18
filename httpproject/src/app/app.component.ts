a3d6e9a5808ecb33ddd511f4b6ef763f581ebcdc
Anthony Scales <ascales@nexient.com>
2018-12-18 16:34:13 -0500

    Made Changes to httpproject

--- httpproject/src/app/app.component.ts
+++ httpproject/src/app/app.component.ts
@@ -6,9 +6,12 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/retry';
 import 'rxjs/add/observable/of';
+<<<<<<< HEAD
 import { last } from "rxjs/operators";
 
 
+=======
+>>>>>>> 0c2ad2c930f500ff20c974f1ab7534421d972f06
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -31,17 +34,28 @@ export class AppComponent {
     let headers = new HttpHeaders().set('Authorization', 'auth-token');
     let params = new HttpParams().set('userId', '1');
 
+<<<<<<< HEAD
     //this.posts = this.http.get(this.Root_URL + '/posts', { headers })
     // this.posts = this.http.get(this.Root_URL + '/posts', { params})
 
     this.posts = this.http.get<Post[]>(this.Root_URL + '/posts')
+=======
+    this.posts = this.http.get(this.Root_URL + '/posts', { headers })
+    // this.posts = this.http.get(this.Root_URL + '/posts', { params})
+
+    // this.posts = this.http.get<Post[]>(this.Root_URL + '/posts?userId=1')
+>>>>>>> 0c2ad2c930f500ff20c974f1ab7534421d972f06
   }
 
   createPost(){
     const data: Post = {
       id: null,
       userId: 23,
+<<<<<<< HEAD
       title: "Post that I added",
+=======
+      title: "Post I I added",
+>>>>>>> 0c2ad2c930f500ff20c974f1ab7534421d972f06
       body: " Hey Buddy"
     } 
 
